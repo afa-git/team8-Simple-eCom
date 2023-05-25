471227cd28ec7116ad5fa1a00091601769cce700
Aditya Yusril Fikri <38427057+yusrilAditya98@users.noreply.github.com>
2023-05-25 08:05:40 +0700

    update init data
    update init data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
